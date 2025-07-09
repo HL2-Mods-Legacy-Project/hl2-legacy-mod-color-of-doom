565396ba622620200c139989d75bf7742361a675
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:59:34 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "ColorOfDoom_1.0" `
+  -ModName "The Color of Doom 1.0" `
+  -ModFolder "ColorOfDeath" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/color-of-doom/Color-of-Doom-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "48a428fad3e8c947523a4a45ff607ec439058155bbce379092fe0a1c73b449d9" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
